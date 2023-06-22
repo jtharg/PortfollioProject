b89b6d4d0b3d86939e4f765fb2ec9fb5b16fc776
jtharg <j.t.hargenrader@email.msmary.edu>
2023-06-21 22:16:29 -0400

    Created the layout of the project page and finished my capstone project section.

--- PortfollioProject/projects.aspx.cs
+++ PortfollioProject/projects.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,41 @@ namespace PortfollioProject
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void downloadDocumentation(object sender, EventArgs e)
+        {
+            try
+            {
+                string resumeFilePath = Server.MapPath("~/Project-Assets/Documentation.docx");
+                string fileName = Path.GetFileName(resumeFilePath);
+
+                if (File.Exists(resumeFilePath))
+                {
+                    byte[] fileBytes = File.ReadAllBytes(resumeFilePath);
+
+                    // Send the file to the user for download
+                    Response.Clear();
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("Content-Disposition", "attachment; filename=" + fileName);
+                    Response.OutputStream.Write(fileBytes, 0, fileBytes.Length);
+                    Response.Flush();
+                    Response.End();
+                }
+                else
+                {
+                    divError.Visible = true;
+                    lblError.Visible = true;
+                    lblError.InnerText = "Sorry, there was a problem trying to download my resume. Please email me, or use the contact form to reach me further.";
+
+                }
+            }
+            catch (Exception ex)
+            {
+                divError.Visible = true;
+                lblError.Visible = true;
+                lblError.InnerText = "Sorry, there was a problem trying to download my resume. Please email me, or use the contact form to reach me further.";
+            }
         }
     }
 }
